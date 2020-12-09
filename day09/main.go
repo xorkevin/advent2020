878927e147c8663863cfb370792e57dff2ca6919
Kevin Wang <kevin@xorkevin.com>
2020-12-08 22:43:03 -0800

    Refactor day09 go

--- day09/main.go
+++ day09/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -22,65 +23,87 @@ func main() {
 			log.Fatal(err)
 		}
 	}()
-
-	target := 731031916
-	buffer := make([]int, 0, 25)
-	buffer2 := []int{}
-	noPart1 := true
-
-	ctr := 0
-	lineCount := 0
 	scanner := bufio.NewScanner(file)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	numStream := make(chan int)
+	doneReadNums := make(chan struct{})
+	go readNums(ctx, doneReadNums, scanner, numStream)
+	targetStream := make(chan int)
+	throughStream := make(chan int)
+	doneScanTarget := make(chan struct{})
+	go scanTarget(ctx, doneScanTarget, numStream, targetStream, throughStream)
+
+	target := <-targetStream
+	fmt.Println("Part 1:", target)
+
+	sum := 0
+	buffer := []int{}
+	for sum != target {
+		if sum < target {
+			k, ok := <-throughStream
+			if !ok {
+				break
+			}
+			sum += k
+			buffer = append(buffer, k)
+		} else {
+			if len(buffer) == 0 {
+				break
+			}
+			sum -= buffer[0]
+			buffer = buffer[1:]
+		}
+	}
+	if sum == target {
+		min, max, ok := minMax(buffer)
+		if ok {
+			fmt.Println("Part 2:", min+max)
+		}
+	}
+
+	cancel()
+	<-doneReadNums
+	<-doneScanTarget
+}
+
+func minMax(b []int) (int, int, bool) {
+	if len(b) == 0 {
+		return 0, 0, false
+	}
+	min := b[0]
+	max := b[0]
+	for _, i := range b {
+		if i < min {
+			min = i
+		}
+		if i > max {
+			max = i
+		}
+	}
+	return min, max, true
+}
+
+func readNums(ctx context.Context, done chan<- struct{}, scanner *bufio.Scanner, s chan<- int) {
+	defer close(done)
+	defer close(s)
+
 	for scanner.Scan() {
 		num, err := strconv.Atoi(scanner.Text())
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			return
 		}
-		if noPart1 {
-			if lineCount < 25 {
-				buffer = append(buffer, num)
-				lineCount++
-			} else {
-				if !hasParts(num, buffer) {
-					fmt.Println("Part 1:", num)
-					noPart1 = false
-				} else {
-					buffer[ctr] = num
-				}
-				ctr = (ctr + 1) % len(buffer)
-			}
+		select {
+		case <-ctx.Done():
+			return
+		case s <- num:
 		}
-		buffer2 = append(buffer2, num)
 	}
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
-
-	sum := 0
-	i := 0
-	j := 0
-	for sum != target {
-		if sum < target {
-			sum += buffer2[j]
-			j++
-		} else {
-			sum -= buffer2[i]
-			i++
-		}
-	}
-
-	smallest := 99999999
-	largest := 0
-	for k := i; k < j; k++ {
-		c := buffer2[k]
-		if c < smallest {
-			smallest = c
-		}
-		if c > largest {
-			largest = c
-		}
-	}
-	fmt.Println("Part 2:", smallest+largest)
 }
 
 func hasParts(a int, buffer []int) bool {
@@ -92,4 +115,61 @@ func hasParts(a int, buffer []int) bool {
 		}
 	}
 	return false
+}
+
+func scanTarget(ctx context.Context, done chan<- struct{}, s <-chan int, target chan<- int, through chan<- int) {
+	defer close(done)
+	defer close(through)
+	defer close(target)
+
+	{
+		preamble := 25
+		buffer := []int{}
+		for preamble > 0 {
+			k, ok := <-s
+			if !ok {
+				return
+			}
+			buffer = append(buffer, k)
+			preamble--
+		}
+
+		idx := 0
+		for {
+			k, ok := <-s
+			if !ok {
+				return
+			}
+			buffer = append(buffer, k)
+			if !hasParts(k, buffer[idx:idx+25]) {
+				select {
+				case <-ctx.Done():
+					return
+				case target <- k:
+				}
+				break
+			}
+			idx++
+		}
+
+		for _, i := range buffer {
+			select {
+			case <-ctx.Done():
+				return
+			case through <- i:
+			}
+		}
+	}
+
+	for {
+		k, ok := <-s
+		if !ok {
+			return
+		}
+		select {
+		case <-ctx.Done():
+			return
+		case through <- k:
+		}
+	}
 }
