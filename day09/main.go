ecb5e38394be4bb68ee070bd1f0db4c5dc7ce6f7
Kevin Wang <kevin@xorkevin.com>
2020-12-09 23:41:06 -0800

    Simplify day09 go

--- day09/main.go
+++ day09/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"fmt"
 	"log"
 	"os"
@@ -25,26 +24,21 @@ func main() {
 	}()
 	scanner := bufio.NewScanner(file)
 
-	ctx, cancel := context.WithCancel(context.Background())
+	r := NewTargetReader(scanner)
 
-	numStream := make(chan int)
-	doneReadNums := make(chan struct{})
-	go readNums(ctx, doneReadNums, scanner, numStream)
-	targetStream := make(chan int)
-	throughStream := make(chan int)
-	doneScanTarget := make(chan struct{})
-	go scanTarget(ctx, doneScanTarget, numStream, targetStream, throughStream)
-
-	target := <-targetStream
+	target, err := r.Scan()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Part 1:", target)
 
 	sum := 0
 	buffer := []int{}
 	for sum != target {
 		if sum < target {
-			k, ok := <-throughStream
-			if !ok {
-				break
+			k, err := r.Next()
+			if err != nil {
+				log.Fatal(err)
 			}
 			sum += k
 			buffer = append(buffer, k)
@@ -62,10 +56,83 @@ func main() {
 			fmt.Println("Part 2:", min+max)
 		}
 	}
+}
 
-	cancel()
-	<-doneReadNums
-	<-doneScanTarget
+type (
+	TargetReader struct {
+		buf     []int
+		scanner *bufio.Scanner
+	}
+)
+
+func NewTargetReader(scanner *bufio.Scanner) *TargetReader {
+	return &TargetReader{
+		buf:     []int{},
+		scanner: scanner,
+	}
+}
+
+func (r *TargetReader) readLine() (int, error) {
+	if !r.scanner.Scan() {
+		if err := r.scanner.Err(); err != nil {
+			return 0, err
+		}
+		return 0, fmt.Errorf("End of file")
+	}
+	num, err := strconv.Atoi(r.scanner.Text())
+	if err != nil {
+		return 0, err
+	}
+	return num, nil
+}
+
+func (r *TargetReader) Scan() (int, error) {
+	preamble := 25
+	b := []int{}
+	for preamble > 0 {
+		k, err := r.readLine()
+		if err != nil {
+			return 0, err
+		}
+		b = append(b, k)
+		preamble--
+	}
+
+	idx := 0
+	for {
+		k, err := r.readLine()
+		if err != nil {
+			return 0, err
+		}
+		b = append(b, k)
+		if !hasParts(k, b[idx:idx+25]) {
+			break
+		}
+		idx++
+	}
+
+	r.buf = b
+	return b[len(b)-1], nil
+}
+
+func (r *TargetReader) Next() (int, error) {
+	if len(r.buf) > 0 {
+		k := r.buf[0]
+		r.buf = r.buf[1:]
+		return k, nil
+	}
+	return r.readLine()
+}
+
+func hasParts(a int, buffer []int) bool {
+	for i := 0; i < len(buffer); i++ {
+		for j := i + 1; j < len(buffer); j++ {
+			if buffer[i]+buffer[j] == a {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func minMax(b []int) (int, int, bool) {
@@ -83,93 +150,4 @@ func minMax(b []int) (int, int, bool) {
 		}
 	}
 	return min, max, true
-}
-
-func readNums(ctx context.Context, done chan<- struct{}, scanner *bufio.Scanner, s chan<- int) {
-	defer close(done)
-	defer close(s)
-
-	for scanner.Scan() {
-		num, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		select {
-		case <-ctx.Done():
-			return
-		case s <- num:
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Print(err)
-	}
-}
-
-func hasParts(a int, buffer []int) bool {
-	for i := 0; i < len(buffer); i++ {
-		for j := i + 1; j < len(buffer); j++ {
-			if buffer[i]+buffer[j] == a {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func scanTarget(ctx context.Context, done chan<- struct{}, s <-chan int, target chan<- int, through chan<- int) {
-	defer close(done)
-	defer close(through)
-	defer close(target)
-
-	{
-		preamble := 25
-		buffer := []int{}
-		for preamble > 0 {
-			k, ok := <-s
-			if !ok {
-				return
-			}
-			buffer = append(buffer, k)
-			preamble--
-		}
-
-		idx := 0
-		for {
-			k, ok := <-s
-			if !ok {
-				return
-			}
-			buffer = append(buffer, k)
-			if !hasParts(k, buffer[idx:idx+25]) {
-				select {
-				case <-ctx.Done():
-					return
-				case target <- k:
-				}
-				break
-			}
-			idx++
-		}
-
-		for _, i := range buffer {
-			select {
-			case <-ctx.Done():
-				return
-			case through <- i:
-			}
-		}
-	}
-
-	for {
-		k, ok := <-s
-		if !ok {
-			return
-		}
-		select {
-		case <-ctx.Done():
-			return
-		case through <- k:
-		}
-	}
 }
