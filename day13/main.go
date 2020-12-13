fe837e4297bbac6827a7ca268f4cf147fe41bc07
Kevin Wang <kevin@xorkevin.com>
2020-12-13 13:29:36 -0800

    Refactor day13

--- day13/main.go
+++ day13/main.go
@@ -13,7 +13,7 @@ const (
 )
 
 func main() {
-	buses := []int{}
+	nums := []int{}
 	rem := []int{}
 
 	for n, i := range strings.Split(puzzleInput2, ",") {
@@ -24,61 +24,71 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		buses = append(buses, num)
+		nums = append(nums, num)
 		rem = append(rem, num-n)
 	}
 
 	for i := puzzleInput; ; i++ {
-		k, ok := canTake(i, buses)
+		k, ok := canTake(i, nums)
 		if ok {
 			fmt.Println("Part 1:", k*(i-puzzleInput))
 			break
 		}
 	}
 
-	fmt.Println("Part 2:", crt(buses, rem))
+	x, err := crt(nums, rem)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Part 2:", x)
 }
 
-func crt(nums, rem []int) int {
+func crt(nums, rem []int) (int, error) {
+	if len(nums) != len(rem) {
+		return 0, fmt.Errorf("Invalid pairs")
+	}
 	p := 1
 	for _, i := range nums {
 		p *= i
 	}
 	k := 0
 	for i := range nums {
-		t := p / nums[i]
-		k += rem[i] * mulInv(t, nums[i]) * t
+		n := p / nums[i]
+		t, err := mulInv(n, nums[i])
+		if err != nil {
+			return 0, err
+		}
+		k += rem[i] * t * n
+		k %= p
 	}
-	return k % p
+	return (k + p) % p, nil
 }
 
-func mulInv(a, b int) int {
-	if b == 1 {
-		return 1
+// mulInv returns t, where a*t = 1 (mod n)
+func mulInv(a, n int) (int, error) {
+	t0 := 0
+	t1 := 1
+	r0 := n
+	r1 := a
+	for r1 != 0 {
+		q := r0 / r1
+		r0, r1 = r1, r0-q*r1
+		t0, t1 = t1, t0-q*t1
 	}
-	b0 := b
-	x0 := 0
-	x1 := 1
-	for a > 1 {
-		q := a / b
-		a, b = b, a%b
-		x1, x0 = x0, x1-q*x0
+	if r0 != 1 {
+		return 0, fmt.Errorf("No mul inverse for %d mod %d", a, n)
 	}
-	if x1 < 0 {
-		return x1 + b0
+	if t0 < 0 {
+		return t0 + n, nil
 	}
-	return x1
+	return t0, nil
 }
 
-func canTake(t int, buses []int) (int, bool) {
-	for _, b := range buses {
-		if canTakeBus(t, b) {
+func canTake(t int, nums []int) (int, bool) {
+	for _, b := range nums {
+		if t%b == 0 {
 			return b, true
 		}
 	}
 	return 0, false
-}
-
-func canTakeBus(t int, b int) bool {
-	return t%b == 0
 }
