57de37be9fb93db323f8c52654b02341f1e1d43a
Kevin Wang <kevin@xorkevin.com>
2020-12-13 22:13:40 -0800

    Refactor day14

--- day14/main.go
+++ day14/main.go
@@ -27,7 +27,10 @@ func main() {
 	mem := map[int]int{}
 	mem2 := map[int]int{}
 
-	var mask []byte
+	maskZeros := 0
+	maskOnes := 0
+	maskFloats := 0
+	var maskPos []int
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -36,7 +39,7 @@ func main() {
 			log.Fatal("Invalid line")
 		}
 		if s[0] == "mask" {
-			mask = []byte(s[1])
+			maskZeros, maskOnes, maskFloats, maskPos = processMask([]byte(s[1]))
 			continue
 		}
 		addr, err := strconv.Atoi(s[0][4 : len(s[0])-1])
@@ -48,9 +51,9 @@ func main() {
 			log.Fatal(err)
 		}
 
-		mem[addr] = applyMask(num, mask)
+		mem[addr] = (num & maskZeros) | maskOnes
 
-		for _, i := range maskAddrs(orMask(addr, mask)) {
+		for _, i := range maskAddrs(maskPos, (addr&maskFloats)|maskOnes) {
 			mem2[i] = num
 		}
 	}
@@ -71,76 +74,32 @@ func main() {
 	fmt.Println("Part 2:", sum2)
 }
 
-func maskAddrs(b []byte) []int {
-	base := 0
-	for n, i := range b {
-		p := len(b) - n - 1
-		if i == '1' {
-			base = setBit(base, p)
-		}
-	}
-	pos := []int{}
-	for n, i := range b {
-		if i != 'X' {
-			continue
-		}
-		pos = append(pos, len(b)-n-1)
-	}
-	return maskAddrsRec(pos, base)
-}
-
-func maskAddrsRec(pos []int, base int) []int {
+func maskAddrs(pos []int, base int) []int {
 	if len(pos) == 0 {
 		return []int{base}
 	}
 	addrs := []int{}
-	addrs = append(addrs, maskAddrsRec(pos[1:], base)...)
-	addrs = append(addrs, maskAddrsRec(pos[1:], setBit(base, pos[0]))...)
+	addrs = append(addrs, maskAddrs(pos[1:], base)...)
+	addrs = append(addrs, maskAddrs(pos[1:], base|1<<pos[0])...)
 	return addrs
 }
 
-func orMask(v int, b []byte) []byte {
-	next := make([]byte, len(b))
+func processMask(b []byte) (int, int, int, []int) {
+	pos := []int{}
+	zeros := 0
+	ones := 0
+	fls := 0
 	for n, i := range b {
 		p := len(b) - n - 1
 		switch i {
 		case '0':
-			if hasBit(v, p) {
-				next[n] = '1'
-			} else {
-				next[n] = '0'
-			}
+			zeros |= 1 << p
 		case '1':
-			next[n] = '1'
+			ones |= 1 << p
 		case 'X':
-			next[n] = 'X'
+			fls |= 1 << p
+			pos = append(pos, p)
 		}
 	}
-	return next
-}
-
-func applyMask(v int, b []byte) int {
-	for n, i := range b {
-		p := len(b) - n - 1
-		switch i {
-		case '0':
-			v = clearBit(v, p)
-		case '1':
-			v = setBit(v, p)
-		}
-	}
-	return v
-}
-
-func setBit(n int, pos int) int {
-	return n | (1 << pos)
-}
-
-func clearBit(n int, pos int) int {
-	return n & ^(1 << pos)
-}
-
-func hasBit(n int, pos int) bool {
-	val := n & (1 << pos)
-	return (val > 0)
+	return ^zeros, ones, ^fls, pos
 }
