f48f1ea1c64abdf5e8b376646c3cb257941059ba
Kevin Wang <kevin@xorkevin.com>
2020-12-23 15:21:00 -0800

    Finish day22

--- day22/main.go
+++ day22/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -13,23 +14,22 @@ const (
 )
 
 type (
-	Card struct {
-		val  int
-		next *Card
-	}
-
 	Deck struct {
-		first *Card
-		last  *Card
-		size  int
+		first    int
+		last     int
+		size     int
+		capacity int
+		data     []int
 	}
 )
 
-func NewDeck() *Deck {
+func NewDeck(capacity int) *Deck {
 	return &Deck{
-		first: nil,
-		last:  nil,
-		size:  0,
+		first:    0,
+		last:     0,
+		size:     0,
+		capacity: capacity + 1,
+		data:     make([]int, capacity+1),
 	}
 }
 
@@ -37,46 +37,42 @@ func (d *Deck) Len() int {
 	return d.size
 }
 
-func (d *Deck) Push(v int) {
-	if d.last == nil {
-		k := &Card{
-			val:  v,
-			next: nil,
-		}
-		d.first = k
-		d.last = k
-	} else {
-		d.last.next = &Card{
-			val:  v,
-			next: nil,
-		}
-		d.last = d.last.next
+func (d *Deck) Push(v int) bool {
+	n := (d.last + 1) % d.capacity
+	if n == d.first {
+		return false
 	}
+	d.data[d.last] = v
+	d.last = n
 	d.size++
+	return true
 }
 
-func (d *Deck) First() int {
-	if d.first == nil {
-		return 0
-	}
-	return d.first.val
-}
-
-func (d *Deck) Pop() int {
+func (d *Deck) Pop() (int, bool) {
 	if d.first == d.last {
-		k := d.first
-		d.first = nil
-		d.last = nil
-		if k != nil {
-			d.size--
-			return k.val
-		}
-		return 0
+		return 0, false
 	}
-	k := d.first
-	d.first = k.next
+	k := d.data[d.first]
+	d.first = (d.first + 1) % d.capacity
 	d.size--
-	return k.val
+	return k, true
+}
+
+func (d *Deck) Copy(n int) *Deck {
+	next := NewDeck(d.capacity)
+	for i := d.first; next.Len() < n; i = (i + 1) % d.capacity {
+		next.Push(d.data[i])
+	}
+	return next
+}
+
+func (d *Deck) String() string {
+	s := &strings.Builder{}
+	for i := d.first; i != d.last; i = (i + 1) % d.capacity {
+		s.WriteString(strconv.Itoa(d.data[i]))
+		s.WriteString(",")
+	}
+	return s.String()
 }
 
 func main() {
@@ -90,10 +86,8 @@ func main() {
 		}
 	}()
 
-	deck11 := NewDeck()
-	deck12 := NewDeck()
-	deck21 := NewDeck()
-	deck22 := NewDeck()
+	cards1 := []int{}
+	cards2 := []int{}
 
 	var mode bool
 	scanner := bufio.NewScanner(file)
@@ -115,38 +109,104 @@ func main() {
 			log.Fatal(err)
 		}
 		if mode {
-			deck12.Push(num)
-			deck22.Push(num)
+			cards2 = append(cards2, num)
 		} else {
-			deck11.Push(num)
-			deck21.Push(num)
+			cards1 = append(cards1, num)
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	for deck11.Len() > 0 && deck12.Len() > 0 {
-		v1 := deck11.Pop()
-		v2 := deck12.Pop()
-		if v1 > v2 {
-			deck11.Push(v1)
-			deck11.Push(v2)
+	{
+		deck1 := NewDeck(len(cards1) + len(cards2))
+		deck2 := NewDeck(len(cards1) + len(cards2))
+		for _, i := range cards1 {
+			deck1.Push(i)
+		}
+		for _, i := range cards2 {
+			deck2.Push(i)
+		}
+		for deck1.Len() > 0 && deck2.Len() > 0 {
+			v1, _ := deck1.Pop()
+			v2, _ := deck2.Pop()
+			if v1 > v2 {
+				deck1.Push(v1)
+				deck1.Push(v2)
+			} else {
+				deck2.Push(v2)
+				deck2.Push(v1)
+			}
+		}
+		sum1 := 0
+		if deck1.Len() == 0 {
+			for n := deck2.Len(); n > 0; n-- {
+				v, _ := deck2.Pop()
+				sum1 += v * n
+			}
 		} else {
-			deck12.Push(v2)
-			deck12.Push(v1)
+			for n := deck1.Len(); n > 0; n-- {
+				v, _ := deck1.Pop()
+				sum1 += v * n
+			}
 		}
+		fmt.Println("Part 1:", sum1)
 	}
+	{
+		deck1 := NewDeck(len(cards1) + len(cards2))
+		deck2 := NewDeck(len(cards1) + len(cards2))
+		for _, i := range cards1 {
+			deck1.Push(i)
+		}
+		for _, i := range cards2 {
+			deck2.Push(i)
+		}
 
-	sum1 := 0
-	if deck11.Len() == 0 {
-		for n := deck12.Len(); n > 0; n-- {
-			sum1 += deck12.Pop() * n
+		sum2 := 0
+		if playGame(deck1, deck2) {
+			for n := deck2.Len(); n > 0; n-- {
+				v, _ := deck2.Pop()
+				sum2 += v * n
+			}
+		} else {
+			for n := deck1.Len(); n > 0; n-- {
+				v, _ := deck1.Pop()
+				sum2 += v * n
+			}
 		}
-	} else {
-		for n := deck11.Len(); n > 0; n-- {
-			sum1 += deck11.Pop() * n
+		fmt.Println("Part 2:", sum2)
+	}
+}
+
+func playGame(deck1 *Deck, deck2 *Deck) bool {
+	states := map[string]struct{}{}
+	for deck1.Len() > 0 && deck2.Len() > 0 {
+		current := deck1.String() + ":" + deck2.String()
+		if _, ok := states[current]; ok {
+			return false
+		}
+		states[current] = struct{}{}
+		v1, _ := deck1.Pop()
+		v2, _ := deck2.Pop()
+		if deck1.Len() >= v1 && deck2.Len() >= v2 {
+			subdeck1 := deck1.Copy(v1)
+			subdeck2 := deck2.Copy(v2)
+			if playGame(subdeck1, subdeck2) {
+				deck2.Push(v2)
+				deck2.Push(v1)
+			} else {
+				deck1.Push(v1)
+				deck1.Push(v2)
+			}
+		} else {
+			if v1 > v2 {
+				deck1.Push(v1)
+				deck1.Push(v2)
+			} else {
+				deck2.Push(v2)
+				deck2.Push(v1)
+			}
 		}
 	}
-	fmt.Println("Part 1:", sum1)
+	return deck1.Len() == 0
 }
